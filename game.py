5c3fd13bef1f8ff52c59129b096af96409ed62ba
brauger <bra237@stern.nyu.edu>
2022-07-12 14:17:42 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,22 @@
 print("Rock, Paper, Scissors, Shoot!")
+
+print("Welcome 'Player One' to the best game you will ever play!")
+
+user = input("Choose rock, paper or scissors: ")
+
+if user in ["rock", "paper", "scissors"]:
+    print("Player One is:" + user)
+else:
+    print("Invalid input")
+    exit()
+
+
+import random
+possible = ["rock", "paper", "scissors"]
+computer = random.choice(possible)
+
+print("The computer is:" + random.choice(possible))
+
+
+
+
