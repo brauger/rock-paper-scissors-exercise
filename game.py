0ec727fd14e50e51c534e60f8ed329a73d7bf8b3
brauger <bra237@stern.nyu.edu>
2022-07-12 20:00:57 -0400

    Update game.py

--- game.py
+++ game.py
@@ -2,7 +2,9 @@ print("Rock, Paper, Scissors, Shoot!")
 
 print("Welcome 'Player One' to the best game you will ever play!")
 
+
 user = input("Choose rock, paper or scissors: ")
+
 
 if user.lower() in ["rock", "paper", "scissors"]:
     print("Player One is:" + user) 
@@ -15,8 +17,29 @@ import random
 possible = ["rock", "paper", "scissors"]
 computer = random.choice(possible)
 
-print("The computer is:" + random.choice(possible))
+print("The computer is: " + computer)
+
+
+
+if user == computer:
+    print("It's a tie!")
+elif user == "rock":
+    if computer == "scissors":
+        print("Rock crushes scissors. You win!")
+    else:
+        print("Paper covers rock. You lose.")
+elif user == "paper":
+    if computer == "rock":
+        print("Paper covers rock. You win!")
+    else:
+        print("Scissors cuts paper. You lose.")
+elif user == "scissors":
+    if computer == "paper":
+        print("Scissors cuts paper. You win!")
+    else:
+        print("Rock crushes scissors. You lose.")
 
 
 
 
+print("Thank you for playing. Hope you had fun!")
